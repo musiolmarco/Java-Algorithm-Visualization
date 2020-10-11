5a43a9feed5e54fcf83a0c974db089f87166c44f
Marco-Musiol <64715619+Marco-Musiol@users.noreply.github.com>
2020-10-12 00:27:22 +0200

    Generating random array order

--- src/AlgorithmVisualizer.java
+++ src/AlgorithmVisualizer.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class AlgorithmVisualizer {
 
     AlgorithmFrame algorithmFrame;
 
     int[] array;
+    ArrayList<Integer> arrayList = new ArrayList<>();
 
     public AlgorithmVisualizer(){
         array = new int[50];
@@ -13,7 +17,15 @@ public class AlgorithmVisualizer {
 
     private void fillArray(){
         for(int i = 0; i < array.length; i++){
-            array[i] = (i + 5) * 7;
+            arrayList.add((i + 5) * 7);
+        }
+
+        Random random = new Random();
+
+        for(int i = 0; i < array.length; i++){
+            int randomIndex = random.nextInt(arrayList.size());
+            array[i] = arrayList.get(randomIndex);
+            arrayList.remove(randomIndex);
         }
     }
 
