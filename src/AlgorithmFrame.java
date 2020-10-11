b23dd5d03482c9c362c72dfad2447380eccd6f23
Marco-Musiol <64715619+Marco-Musiol@users.noreply.github.com>
2020-10-11 22:16:43 +0200

    AlgorithmFrame Class with Constructor

---
+++ src/AlgorithmFrame.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class AlgorithmFrame extends JFrame {
+
+    public AlgorithmFrame(){
+
+        setLayout(new FlowLayout());
+        setVisible(true);
+
+
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    // main method for testing
+    public static void main(String[] args) {
+        AlgorithmFrame algorithmFrame = new AlgorithmFrame();
+    }
+
+}
