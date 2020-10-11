72504c01d884e80e8634dac81ec186b6c32a0f63
Marco-Musiol <64715619+Marco-Musiol@users.noreply.github.com>
2020-10-11 22:36:26 +0200

    Created AlgorithmVisualizer class

---
+++ src/AlgorithmVisualizer.java
@@ -0,0 +1,9 @@
+public class AlgorithmVisualizer {
+
+    AlgorithmFrame algorithmFrame;
+
+    public AlgorithmVisualizer(int width, int height){
+        algorithmFrame = new AlgorithmFrame(width, height);
+    }
+
+}
