a6b2854d2d8522ed55d499992af349633c465010
Marco-Musiol <64715619+Marco-Musiol@users.noreply.github.com>
2020-10-11 22:29:02 +0200

    Created 'ValueBars' class and added to AlgorithmFrame

--- src/AlgorithmFrame.java
+++ src/AlgorithmFrame.java
@@ -3,10 +3,18 @@ import java.awt.*;
 
 public class AlgorithmFrame extends JFrame {
 
-    public AlgorithmFrame(){
+
+
+    public AlgorithmFrame(int width, int height){
 
         setLayout(new FlowLayout());
         setVisible(true);
+        setSize(width,height);
+
+        ValueBars valueBars = new ValueBars();
+        valueBars.setBounds(0,0,width,height);
+        valueBars.setVisible(true);
+        add(valueBars);
 
 
     }
