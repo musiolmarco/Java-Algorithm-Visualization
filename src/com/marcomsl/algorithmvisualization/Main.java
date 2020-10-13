34bd98c63b05d5a73155194b5a121c3f6f44be27
Marco-Musiol <64715619+MarcoMusiol@users.noreply.github.com>
2020-10-13 22:17:51 +0200

    Added Colors.png

--- src/com/marcomsl/algorithmvisualization/Main.java
+++ src/com/marcomsl/algorithmvisualization/Main.java
@@ -5,6 +5,10 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         AlgorithmVisualizer algorithmVisualizer = new AlgorithmVisualizer();
+        AlgorithmVisualizer visualizer = new AlgorithmVisualizer(BarColor.RED);
+        AlgorithmVisualizer visualizer1 = new AlgorithmVisualizer(BarColor.GREEN);
+        AlgorithmVisualizer visualizer2 = new AlgorithmVisualizer(BarColor.BLUE);
+
     }
 
 }
