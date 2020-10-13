0e33d7227ce2e6fab02f636bf2f4a60bd02a5ebe
Marco-Musiol <64715619+MarcoMusiol@users.noreply.github.com>
2020-10-13 22:12:02 +0200

    Added Banner.png

---
+++ src/com/marcomsl/algorithmvisualization/Main.java
@@ -0,0 +1,10 @@
+package com.marcomsl.algorithmvisualization;
+
+public class Main {
+
+
+    public static void main(String[] args) throws InterruptedException {
+        AlgorithmVisualizer algorithmVisualizer = new AlgorithmVisualizer();
+    }
+
+}
--- src/com/marcomsl/algorithmvisualization/Mainj.java
+++
@@ -1,9 +0,0 @@
-package com.marcomsl.algorithmvisualization;
-
-public class Mainj {
-
-    public static void main(String[] args) throws InterruptedException {
-        AlgorithmVisualizer algorithmVisualizer = new AlgorithmVisualizer(BarColor.BLUE);
-        algorithmVisualizer.bubblesort(50);
-    }
-}
